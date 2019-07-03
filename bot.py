17f0990e95cfa7c8b1793f31559bfe6d3000f29a
Sam <sam-marx@protonmail.com>
2019-07-03 12:48:48 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -18,12 +18,6 @@ import re
 import datetime
 import socket
 from pytz import timezone
-import shutil
-from nudity import Nudity
-from PIL import Image
-import Algorithmia
-
-nudity = Nudity()
 
 #BOT TOKEN
 API_TOKEN = os.getenv('TOKEN')
@@ -37,12 +31,6 @@ REG_GROUP = os.getenv('REGISTER')
 #MODO = DEV / PROD
 modo = os.getenv('MODO')
 
-#ALGORITHMIA NUDITY DETECTION
-ALGORITHMIA_KEY = os.getenv('ALGO_KEY')
-client = Algorithmia.client(ALGORITHMIA_KEY)
-algo = client.algo('sfw/NudityDetection/1.1.6')
-algo.set_options(timeout=300)
-
 #SHODAN KEYS
 shod_key1 = os.getenv('SHODAN_1')
 shod_key2 = os.getenv('SHODAN_2')
@@ -55,11 +43,6 @@ canal = ['https://t.me/AcervoDoSam', '@AcervoDoSam']
 ####
 
 permitidos = ['-1001480767444']
-Excecoes = ['']
-
-ids = open('ids.txt', 'r')
-for i in ids:
-	Excecoes.append(str(i))
 
 logging.basicConfig(level=logging.INFO)
 
@@ -95,143 +78,10 @@ def check_adm(user_id, admin_list):
 	return False
 
 @run_async
-def check_nude_sticker(bot, update):
-	if check_group(chat_id=update.message.chat_id) == True:
-		'''
-		A função check_nude_sticker será a versão 1.0 (sem Algorithmia), pois
-		não é possível fazer a verificação com a extensão .webp.
-		'''
-		try:
-			sticker = str(bot.get_file(update.message.sticker.file_id)).replace("'", '"')
-
-			js = json.loads(sticker)
-			sticker = js['file_path']
-
-			r = requests.get(sticker, stream=True)
-			sticker = sticker.split('/')[-1]
-
-			with open(sticker, 'wb') as fo:
-				shutil.copyfileobj(r.raw, fo)
-			del r
-
-			#transformando de webp para jpg
-			im = Image.open(sticker).convert('RGB') 
-			sticker = sticker.split('.')[0] + '.jpg'
-			im.save(sticker, 'jpeg')
-
-			if nudity.has(str(sticker)) == True: #checando se tem nude ou n com nudepy
-				alvo_id = update.message.from_user.id
-				alvo_usuario = update.message.from_user.username
-				alvo_nome = update.message.from_user.first_name
-
-				if str(alvo_id) not in Excecoes:
-					if alvo_usuario == None:
-						banido = '<b>Usuário {} - {} banido por envio de pornografia.</b>'.format(alvo_nome, alvo_id)
-					else:
-						banido = '<b>Usuário {} - {} banido por envio de pornografia.</b>'.format(alvo_usuario, alvo_id)
-
-					banido_usuario = '''
-#BANIDO_USUARIO
-<b>Usuário: </b>{user_name} [<a href="{link}">{user_id}</a>]
-<b>Grupo: </b>{group_name} [{group_id}]
-<b>Data: </b>{data}
-<b>Motivo: </b>{motivo}
-'''.format(user_name=alvo_usuario,
-					user_id=alvo_id,
-					group_name=update.message.chat.title,
-					group_id=update.message.chat.id,
-					data=datetime.datetime.now(timezone('America/Sao_Paulo')).strftime('%H:%M %d %B, %Y'),
-					motivo='Pornografia',
-					link='tg://user?id=' + str(alvo_id))
-
-					bot.kick_chat_member(chat_id=update.message.chat_id, user_id=alvo_id)
-					bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text=banido, reply_to_message_id=update.message.message_id)
-					bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
-					bot.send_message(parse_mode='HTML', chat_id=REG_GROUP, text=banido_usuario)
-
-					del banido_usuario
-					del banido
-					del alvo_id
-					del alvo_usuario
-					del alvo_nome
-			else: pass
-
-			os.remove(sticker) #remove os stickers salvos para n ocupar espaço
-			os.remove(sticker.split('.')[0] + '.webp') #remove os stickers salvos para n ocupar espaço
-		
-			del sticker
-		except BadRequest as e:
-			try:
-				os.remove(sticker)
-				os.remove(sticker.split('.')[0] + '.webp')
-			except: pass
-
-			bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text='<b>O usuário é administrador e não pode ser banido.</b>', reply_to_message_id=update.message.message_id)
-			bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
-
-@run_async
-def check_nude_image(bot, update):
-	if check_group(chat_id=update.message.chat_id) == True:
-		try:
-			foto = bot.get_file(update.message.photo[-1].file_id)
-
-			foto = str(foto) #transformando o tipo type para string
-			foto = foto.replace("'", '"') #substituindo os ' por "
-
-			js = json.loads(foto) #carregando o json
-			foto = js['file_path'] #conseguindo a fonte da imagem
-
-			algo_js = algo.pipe(foto).result #passando a fonte na API do algorithmia
-
-			if algo_js['nude'] == 'true': #checando se tem nude ou n
-				alvo_id = update.message.from_user.id
-				alvo_usuario = update.message.from_user.username
-				alvo_nome = update.message.from_user.first_name
-
-				if str(alvo_id) not in Excecoes:
-					if alvo_usuario == None:
-						banido = '<b>Usuário {} - {} banido por envio de pornografia.</b>'.format(alvo_nome, alvo_id)
-					else:
-						banido = '<b>Usuário {} - {} banido por envio de pornografia.</b>'.format(alvo_usuario, alvo_id)
-
-					banido_usuario = '''
-#BANIDO_USUARIO
-<b>Usuário: </b>{user_name} [<a href="{link}">{user_id}</a>]
-<b>Grupo: </b>{group_name} [{group_id}]
-<b>Data: </b>{data}
-<b>Motivo: </b>{motivo}
-'''.format(user_name=alvo_usuario,
-					user_id=alvo_id,
-					group_name=update.message.chat.title,
-					group_id=update.message.chat.id,
-					data=datetime.datetime.now(timezone('America/Sao_Paulo')).strftime('%H:%M %d %B, %Y'),
-					motivo='Pornografia',
-					link='tg://user?id=' + str(alvo_id))
-
-					bot.kick_chat_member(chat_id=update.message.chat_id, user_id=alvo_id)
-					bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text=banido, reply_to_message_id=update.message.message_id)
-					bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
-					bot.send_message(parse_mode='HTML', chat_id=REG_GROUP, text=banido_usuario)
-
-					del banido_usuario
-					del banido
-					del alvo_id
-					del alvo_usuario
-					del alvo_nome
-			else: pass
-
-			#tentando liberar memória pro bot n explodir
-			del js
-			del algo_js
-
-		except BadRequest as e:
-			bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text='<b>O usuário é administrador e não pode ser banido.</b>', reply_to_message_id=update.message.message_id)
-			bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
-
-@run_async
 def saida(bot, update):
 	if check_group(chat_id=update.message.chat_id) == True:
 		m = update.message.left_chat_member
+		ids_txt = open('ids.txt', 'r+')
 
 		if m is not None:
 			if m.is_bot == True:
@@ -267,13 +117,14 @@ def saida(bot, update):
 def bvindas(bot, update):
 	if check_group(chat_id=update.message.chat_id) == True:
 		for m in update.message.new_chat_members:
+			ids_txt = open('ids.txt', 'r+')
 			alvo_id = m.id
 
 			if m.full_name == None or m.first_name == None:
 				m.full_name = m.username
 
 			if m.is_bot == True:
-				if str(alvo_id) not in Excecoes:
+				if str(alvo_id) not in ids_txt.readlines():
 					bot.kick_chat_member(chat_id=update.message.chat_id, user_id=alvo_id)
 					bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text='<b>Bot {} removido.</b>'.format(alvo_id))
 
@@ -367,15 +218,17 @@ def excecoes(bot, update, args):
 
 	if check_adm(user_id=user_id, admin_list=admin_list) == True:
 		try:
-			ids_txt = open('ids.txt', 'r+').readlines()
+			ids_txt = open('ids.txt', 'r+')
 
-			for tg_id in ids_txt:
+			for tg_id in ids_txt.readlines():
 				if str(tg_id) == str(args[0]):
 					bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text='<b>O ID {} já é uma exceção.</b>'.format(args[0]), reply_to_message_id=update.message.message_id)
+					break
 				else:
 					ids_txt.write('\n' + str(args[0]))
 					bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text='<b>O ID {} agora é uma exceção e pode ser adicionado ao grupo.</b>'.format(args[0]), reply_to_message_id=update.message.message_id)
-		
+				break
+
 			ids_txt.close()
 		except Exception as e:
 			print('Erro: ' + str(e))
@@ -740,13 +593,7 @@ def main():
 
 	# saida
 	dispatcher.add_handler(MessageHandler(Filters.status_update.left_chat_member, saida))
-	
-	# filtrar imagens
-	dispatcher.add_handler(MessageHandler(Filters.photo, check_nude_image))
 
-	# filtrar stickers
-	dispatcher.add_handler(MessageHandler(Filters.sticker, check_nude_sticker))
-	
 	rodar(updater)
 
 if __name__ == '__main__':
